f174a82c7d795bd0188008f37a3f2caf14133d57
supertopher <asimplechris@gmail.com>
2013-07-01 13:27:34 -0700

    JS logic for victory added.  Images, routes, and erb for victory addded

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,11 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/victory_dino.html' do
+  erb :dino
+end
+
+get '/victory_jeep.html' do
+  erb :jeep
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-  $(function () {
-    $(document).keyup(function (e) {
-      if(keyCode === 61){}
-   });
+  var dinoCount = 1;
+  var jeepCount = 1;
+  $(document).keyup(function (e) {
+    if (e.keyCode == 32) {
+      dinoCount = dinoCount + 1
+      var dino = $('#dino td.active');
+      $(dino).removeClass('active');
+      $(dino).next().addClass('active');
+        location.replace("/victory_dino.html");
+
+    }
+    if (e.keyCode == 39) {
+      jeepCount = jeepCount + 1;
+      if (jeepCount > 10){
+        console.log(jeepCount);
+        location.replace("/victory_jeep.html");
+      }
+      var jeep = $('#jeep td.active');
+      $(jeep).removeClass('active');
+      $(jeep).next().addClass('active');
+    }
   });
 });
